2ccaaa5e88c32938c55a0eb396d372c6248eff7d
JrzenonDev <jrobertoonb@gmail.com>
2023-10-12 01:27:03 -0300

    feat: create component FormTodo

---
+++ src/components/FormTodo.vue
@@ -0,0 +1,14 @@
+<template>
+  <form @submit.prevent="addTodo">
+    <div class="field is-grouped mb-5">
+      <p class="control is-expanded">
+        <input class="input" type="text" placeholder="Add a todo" />
+      </p>
+      <p class="control">
+        <a class="button is-info"> Add </a>
+      </p>
+    </div>
+  </form>
+</template>
+
+<script setup></script>
