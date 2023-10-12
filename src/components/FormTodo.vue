a1b45dc3bc73aca10814eb004f408649dc4b85de
JrzenonDev <jrobertoonb@gmail.com>
2023-10-12 02:05:56 -0300

    feat: init method addTodo use uuid and passing todo to ListTodo

--- src/components/FormTodo.vue
+++ src/components/FormTodo.vue
@@ -2,13 +2,36 @@
   <form @submit.prevent="addTodo">
     <div class="field is-grouped mb-5">
       <p class="control is-expanded">
-        <input class="input" type="text" placeholder="Add a todo" />
+        <input v-model="newTodoContent" class="input" type="text" placeholder="Add a todo" />
       </p>
       <p class="control">
-        <a class="button is-info"> Add </a>
+        <button :disabled="!newTodoContent" class="button is-info">Add</button>
       </p>
     </div>
   </form>
+  <ListTodo :todos="todos" />
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { v4 as uuidv4 } from 'uuid'
+import ListTodo from './ListTodo.vue'
+
+const todos = ref([
+  { id: 1, content: 'Todo 1', done: true },
+  { id: 2, content: 'Todo 2', done: false },
+  { id: 3, content: 'Todo 3', done: false }
+])
+
+const newTodoContent = ref('')
+const addTodo = () => {
+  console.log('add todo')
+  const newTodo = {
+    id: uuidv4(),
+    content: newTodoContent.value,
+    done: false
+  }
+  console.log(newTodo)
+  //todos.value.unshift(newTodo)
+}
+</script>
