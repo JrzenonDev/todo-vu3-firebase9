92f5a669f8e666c1eb3d3fb37f1a277dd623af93
JrzenonDev <jrobertoonb@gmail.com>
2023-10-12 01:27:27 -0300

    feat: create component ListTodo

---
+++ src/components/ListTodo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-for="todo in todos" :key="todo.id" class="card mb-5">
+    <div class="card-content">
+      <div class="content">
+        <div class="columns is-mobile is-vcentered">
+          <div class="column">{{ todo.content }}</div>
+
+          <div class="column has-text-right">
+            <button class="button is-light">&check;</button>
+            <button class="button is-danger ml-1">&cross;</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const todos = ref([
+  { id: 1, content: 'Todo 1', done: true },
+  { id: 2, content: 'Todo 2', done: false },
+  { id: 3, content: 'Todo 3', done: false }
+])
+</script>
