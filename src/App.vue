75d19e552163949dec51a9ca6c9623df9761f4f5
JrzenonDev <jrobertoonb@gmail.com>
2023-10-12 01:28:23 -0300

    refact: separate code in components, FormTodo and LisTodo

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
+<script setup>
+import FormTodo from './components/FormTodo.vue'
+import ListTodo from './components/ListTodo.vue'
+</script>
+
 <template>
   <div class="badass-todo">
     <div class="title has-text-centered">
       <h1>Badass Todo</h1>
     </div>
 
-    <div class="field is-grouped mb-5">
-      <p class="control is-expanded">
-        <input class="input" type="text" placeholder="Add a todo" />
-      </p>
-      <p class="control">
-        <a class="button is-info"> Add </a>
-      </p>
-    </div>
+    <FormTodo />
 
-    <div v-for="i in 3" class="card mb-5">
-      <div class="card-content">
-        <div class="content">
-          <div class="columns is-mobile is-vcentered">
-            <div class="column">Todo 1</div>
-
-            <div class="column has-text-right">
-              <button class="button is-light">&check;</button>
-              <button class="button is-danger ml-1">&cross;</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <ListTodo />
   </div>
 </template>
 
